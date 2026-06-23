e549166f9d009938866bc4cb49fdedc68d9b68a3
NikitaSukharev <niita5505@icloud.com>
2026-06-23 20:40:45 +0300

    fix flake8 warnings

--- page_analyzer/url_validator.py
+++ page_analyzer/url_validator.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse
 
 def normalize_url(url):
     """Нормализует указанный URL"""
-    
+
     parsed_url = urlparse(url)
     scheme = parsed_url.scheme.lower()
     netloc = parsed_url.netloc.lower()
@@ -13,9 +13,9 @@ def normalize_url(url):
 
 def is_valid_url(url):
     """Проверяет, что URL валиден и длина не превышает 255 символов."""
-    
+
     if len(url) > 255:
         return False
     parsed = urlparse(url)
-    
+
     return all([parsed.scheme in ("http", "https"), parsed.netloc])
